0d548249c5a3624612a7288155af6760f76fe7ef
Szymek Radziszewski <szymon.radziszewski93@gmail.com>
2020-05-27 00:13:39 +0200

    Add basics components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PostsComponent } from './posts/posts.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
+import { PostDetailsComponent } from './posts/post-details/post-details.component';
+import { PostItemComponent } from './posts/post-list/post-item/post-item.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsComponent,
+    PostListComponent,
+    PostDetailsComponent,
+    PostItemComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule
