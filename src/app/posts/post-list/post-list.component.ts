f03162f2ca2ba5baca19da0de5df4b25c2c4dae7
Szymek Radziszewski <szymon.radziszewski93@gmail.com>
2020-05-28 23:17:37 +0200

    Add post list layout and dummy data

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Post} from '../post.model';
 
 @Component({
   selector: 'app-post-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-list.component.css']
 })
 export class PostListComponent implements OnInit {
+
+  posts: Post[] = [
+    new Post("Title of post", "Szymek", "Content of this interesting post", "yesterday"),
+    new Post("Another title", "Ptys", "Meow meow meow meow", "tomorrow"),
+  ];
 
   constructor() { }
 
