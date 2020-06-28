62d9390a8ae0b20fe6658890d886dc6e5ec55138
Szymek Radziszewski <szymon.radziszewski93@gmail.com>
2020-06-29 00:24:18 +0200

    create form group and sync ts with html

--- src/app/posts/post-edit/post-edit.component.ts
+++ src/app/posts/post-edit/post-edit.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute, Params} from '@angular/router';
-import {FormGroup} from '@angular/forms';
+import {FormControl, FormGroup} from '@angular/forms';
+import {PostService} from '../post.service';
 
 @Component({
   selector: 'app-post-edit',
@@ -9,22 +10,37 @@ import {FormGroup} from '@angular/forms';
 })
 export class PostEditComponent implements OnInit {
 
-  postEditForm: FormGroup;
+  postForm: FormGroup;
   id: number;
   editMode: boolean = false;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private postService: PostService) { }
 
   ngOnInit(): void {
     this.route.params.subscribe(
       (params: Params) => {
         this.id = +params['id'];
         this.editMode = params['id'] != null;
+        this.initForm();
       }
-    )
-    this.postEditForm = new FormGroup({
+    );
+  }
+  onSubmit(){
+    console.log(this.postForm.value);
+  }
 
-    })
+  private initForm() {
+    let postTitle = '';
+    let postContent = '';
+    if(this.editMode) {
+      postTitle = this.postService.getPost(this.id).title;
+      postContent = this.postService.getPost(this.id).content;
+    }
+
+    this.postForm = new FormGroup({
+      'title': new FormControl(postTitle),
+      'content': new FormControl(postContent),
+    });
   }
 
 }
